8075388b3645d067ba9880076696761ab660c455
David Portes <daporte21@gmail.com>
2025-11-28 16:12:27 +0100

    remove safe range

--- keyboards/bastardkb/tbkmini/keymaps/my_keymap/keymap.c
+++ keyboards/bastardkb/tbkmini/keymaps/my_keymap/keymap.c
@@ -165,7 +165,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
         KC_LSFT, KC_SCLN, KC_Q, KC_J, KC_K, KC_X, KC_B, KC_M, KC_W, KC_V, KC_Z, KC_BSLS,
         //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-        KC_BSPC, KC_TAB, MO(L_NUMBERS), QK_UNDERGLOW_VALUE_UP, KC_SPC, KC_ENT
+        KC_BSPC, KC_TAB, MO(L_NUMBERS), HWNZ, KC_SPC, KC_ENT
         //`--------------------------'  `--------------------------'
 
         ),
