69af0c4809ffccdbe92105938a1814b2d7bc8e8d
David Portes <daporte21@gmail.com>
2025-11-29 10:24:09 +0100

    add steno

--- keyboards/bastardkb/tbkmini/keymaps/my_keymap/rules.mk
+++ keyboards/bastardkb/tbkmini/keymaps/my_keymap/rules.mk
@@ -3,6 +3,8 @@ SRC += num_word.c
 VIA_ENABLE = yes
 COMBO_ENABLE = yes
 NKRO_ENABLE = yes
+STENO_ENABLE = yes
+STENO_PROTOCOL = geminipr
 
 OPT_DEFS += -DUSER_NUM_WORD_ENABLE
 SRC += process_records.c
