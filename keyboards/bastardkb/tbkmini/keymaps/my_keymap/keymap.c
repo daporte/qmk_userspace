512dd348831a5448e0c1b3d5f6c61e47fd815267
David Portes <daporte21@gmail.com>
2025-12-17 15:41:04 +0100

    add switch back to base

--- keyboards/bastardkb/tbkmini/keymaps/my_keymap/keymap.c
+++ keyboards/bastardkb/tbkmini/keymaps/my_keymap/keymap.c
@@ -130,7 +130,7 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
                     // Change all LEDs to red when shift is held
                     rgb_matrix_set_color(i, RGB_GOLD); // R,G,B
                 } else {
-                    rgb_matrix_set_color(i, RGB_RED);
+                    rgb_matrix_set_color(i, RGB_ORANGE);
                 }
                 break;
             case L_BASE_NOMODS:
@@ -188,7 +188,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         MI_C1, MI_Cs1, MI_D1, MI_Ds1, MI_E1, MI_F1, MI_Fs1, MI_G1, MI_Gs1, MI_A1, MI_As1, MI_B1,
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
         MI_C2, MI_Cs2, MI_D2, MI_Ds2, MI_E2, MI_F2, MI_Fs2, MI_G2, MI_Gs2, MI_A2, MI_As2, MI_B2,
-        MI_C3, MI_Cs3, MI_D3, MI_Ds3, MI_E3, MI_F3
+        MI_C3, MI_Cs3, MI_D3, TO(L_BASE), MI_E3, MI_F3
     ),
     [L_GEMINI] = LAYOUT_split_3x6_3(
             TO(L_BASE),  STN_N2,  STN_N3,  STN_N4,  STN_N5,  STN_N6,  STN_N7,  STN_N8,  STN_N9,  STN_NA,  STN_NB,  STN_NC ,
