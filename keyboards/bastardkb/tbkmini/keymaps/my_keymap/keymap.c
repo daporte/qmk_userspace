598d0c8508b1477831686bee64916649f23f788c
David Portes <daporte21@gmail.com>
2025-12-17 20:44:03 +0100

    add mods

--- keyboards/bastardkb/tbkmini/keymaps/my_keymap/keymap.c
+++ keyboards/bastardkb/tbkmini/keymaps/my_keymap/keymap.c
@@ -195,7 +195,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         MI_C1, MI_Cs1, MI_D1, MI_Ds1, MI_E1, MI_F1, MI_Fs1, MI_G1, MI_Gs1, MI_A1, MI_As1, MI_B1,
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
         MI_C2, MI_Cs2, MI_D2, MI_Ds2, MI_E2, MI_F2, MI_Fs2, MI_G2, MI_Gs2, MI_A2, MI_As2, MI_B2,
-        MI_C3, MI_Cs3, MI_D3, TO(L_BASE), MI_E3, MI_F3
+        LSFT_T(MI_C3), LCTL_T(MI_Cs3), MI_D3, TO(L_BASE), MI_E3, RALT_T(MI_F3)
     ),
     [L_GEMINI] = LAYOUT_split_3x6_3(
             TO(L_BASE),  STN_N2,  STN_N3,  STN_N4,  STN_N5,  STN_N6,  STN_N7,  STN_N8,  STN_N9,  STN_NA,  STN_NB,  STN_NC ,
