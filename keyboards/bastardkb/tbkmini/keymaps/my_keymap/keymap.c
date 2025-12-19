1c443a51777545a53c5531f002c090781e62e5b5
David Portes <daporte21@gmail.com>
2025-12-19 12:00:35 +0100

    add jkwb

--- keyboards/bastardkb/tbkmini/keymaps/my_keymap/keymap.c
+++ keyboards/bastardkb/tbkmini/keymaps/my_keymap/keymap.c
@@ -197,7 +197,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
         MI_C1, MI_Cs1, MI_D1, MI_Ds1, MI_E1, MI_F1, MI_Fs1, MI_G1, MI_Gs1, MI_A1, MI_As1, MI_B1,
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        MI_C2, MI_Cs2, MI_D2, MI_Ds2, MI_E2, MI_F2, MI_Fs2, MI_G2, MI_Gs2, MI_A2, MI_As2, MI_B2,
+        MI_C2, MI_Cs2, MI_D2, KC_J, KC_K, MI_F2, KC_B, MI_G2, KC_W, MI_A2, MI_As2, MI_B2,
         LT(0,KC_F22), LT(0, KC_F23), LSFT_T(MI_C3), TO(L_BASE), MI_E3, LT(0, KC_F24)
     ),
     [L_GEMINI] = LAYOUT_split_3x6_3(
