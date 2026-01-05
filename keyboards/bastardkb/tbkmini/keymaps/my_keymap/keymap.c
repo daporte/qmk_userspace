1f54faef5a84696567005c253321878c6222ec23
David Portes <daporte21@gmail.com>
2026-01-05 12:09:44 +0100

    revert wrong cahnges

--- keyboards/bastardkb/tbkmini/keymaps/my_keymap/keymap.c
+++ keyboards/bastardkb/tbkmini/keymaps/my_keymap/keymap.c
@@ -198,7 +198,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         MI_C1, MI_Cs1, MI_D1, MI_Ds1, MI_E1, MI_F1, MI_Fs1, MI_G1, MI_Gs1, MI_A1, MI_As1, MI_B1,
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
         MI_C2, MI_Cs2, MI_D2, KC_J, KC_K, MI_F2, KC_B, MI_G2, KC_W, MI_A2, MI_As2, MI_B2,
-        KC_BSPC, KC_TAB, LSFT_T(MI_C3), TO(L_BASE), MI_E3, LT(0, KC_F24)
+        LT(0,KC_F22), LT(0, KC_F23), LSFT_T(MI_C3), TO(L_BASE), MI_E3, LT(0, KC_F24)
     ),
     [L_GEMINI] = LAYOUT_split_3x6_3(
             STN_N1,  STN_N2,  STN_N3,  STN_N4,  STN_N5,  STN_N6,  STN_N7,  STN_N8,  STN_N9,  STN_NA,  STN_NB,  TO(L_BASE),
