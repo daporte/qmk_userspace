7d46a3602546d8b9170a46665aef3522d44dfdab
David Portes <daporte21@gmail.com>
2026-04-17 10:37:24 +0200

    add keyboard steno layer

--- keyboards/bastardkb/tbkmini/keymaps/my_keymap/keymap.c
+++ keyboards/bastardkb/tbkmini/keymaps/my_keymap/keymap.c
@@ -31,7 +31,6 @@
 /*#define COMBO_TERM 18*/
 
 
-
 enum combos {
     COMBO_QMARK,
     COMBO_EXCL,
@@ -139,8 +138,8 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
             case L_NUMBERS:
                 rgb_matrix_set_color(i, RGB_YELLOW / 10);
                 break;
-            case L_GEMINI:
-                rgb_matrix_set_color(i, RGB_GREEN / 10);
+            case L_STENO:
+                rgb_matrix_set_color(i, RGB_ORANGE / 10);
                 break;
             case L_MIDI:
                 rgb_matrix_set_color(i, RGB_TEAL / 10);
@@ -158,7 +157,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [L_BASE] = LAYOUT_split_3x6_3(
         //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-        KC_DEL, KC_QUOTE, KC_COMM, KC_DOT, KC_P, KC_Y, KC_F, KC_G, KC_C, KC_R, KC_L, TO(L_GEMINI),
+        KC_DEL, KC_QUOTE, KC_COMM, KC_DOT, KC_P, KC_Y, KC_F, KC_G, KC_C, KC_R, KC_L, TO(L_STENO),
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
         KC_ESC, KC_A, KC_O, KC_E, KC_U, KC_I, KC_D, KC_H, KC_T, KC_N, KC_S, KC_SLSH,
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
@@ -200,12 +199,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         MI_C2, MI_Cs2, KC_Q, KC_J, KC_K, MI_F2, KC_B, MI_G2, KC_W, MI_A2, MI_As2, MI_B2,
         LT(0,KC_F22), LT(0, KC_F23), LSFT_T(MI_C3), TO(L_BASE), MI_E3, LT(0, KC_F24)
     ),
-    [L_GEMINI] = LAYOUT_split_3x6_3(
-            STN_N1,  STN_N2,  STN_N3,  STN_N4,  STN_N5,  STN_N6,  STN_N7,  STN_N8,  STN_N9,  STN_NA,  STN_NB,  TO(L_BASE),
-            XXXXXXX, STN_S1,  STN_TL,  STN_PL,  STN_HL,  STN_ST1, STN_ST3, STN_FR,  STN_PR,  STN_LR,  STN_TR,  STN_DR ,
-            XXXXXXX, STN_S2,  STN_KL,  STN_WL,  STN_RL,  STN_ST2, STN_ST4, STN_RR,  STN_BR,  STN_GR,  STN_SR,  STN_ZR ,
-                                       STN_A,   STN_O,   XXXXXXX, XXXXXXX, STN_E,   STN_U
-        ),
+    // [L_GEMINI] = LAYOUT_split_3x6_3(
+    //         STN_N1,  STN_N2,  STN_N3,  STN_N4,  STN_N5,  STN_N6,  STN_N7,  STN_N8,  STN_N9,  STN_NA,  STN_NB,  TO(L_BASE),
+    //         XXXXXXX, STN_S1,  STN_TL,  STN_PL,  STN_HL,  STN_ST1, STN_ST3, STN_FR,  STN_PR,  STN_LR,  STN_TR,  STN_DR ,
+    //         XXXXXXX, STN_S2,  STN_KL,  STN_WL,  STN_RL,  STN_ST2, STN_ST4, STN_RR,  STN_BR,  STN_GR,  STN_SR,  STN_ZR ,
+    //                                    STN_A,   STN_O,   XXXXXXX, XXXXXXX, STN_E,   STN_U
+    //     ),
+    [L_STENO] = LAYOUT_split_3x6_3(
+        KC_F24, KC_F24, KC_F24, KC_F24, KC_F24, KC_F24, KC_F24, KC_F24, KC_F24, KC_F24, KC_F24, TO(L_BASE),
+
+        XXXXXXX, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11,
+
+        XXXXXXX, KC_F12, KC_F13, KC_F14, KC_F15, KC_F16, KC_F17, KC_F18, KC_F19, KC_F20, KC_F21, KC_F22,
+
+                            KC_F23, KC_F23, XXXXXXX, XXXXXXX, KC_F22, KC_F21
+    ),
     [L_BASE_TOGGLE] = LAYOUT_split_3x6_3(
         //,-----------------------------------------------------.                    ,-----------------------------------------------------.
         XXXXXXX, KC_QUOTE, KC_COMM, KC_DOT, KC_P, KC_Y, KC_F, KC_G, KC_C, KC_R, KC_L, KC_DEL,
