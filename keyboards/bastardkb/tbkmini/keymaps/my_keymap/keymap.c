2e180715e9428d64072dac4845e94daa1205c59f
David Portes <daporte21@gmail.com>
2026-04-19 11:09:59 +0200

    fix bracket

--- keyboards/bastardkb/tbkmini/keymaps/my_keymap/keymap.c
+++ keyboards/bastardkb/tbkmini/keymaps/my_keymap/keymap.c
@@ -229,7 +229,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             XXXXXXX, STN_S1,  STN_TL,  STN_PL,  STN_HL,  STN_ST1, STN_ST3, STN_FR,  STN_PR,  STN_LR,  STN_TR,  STN_DR ,
             XXXXXXX, STN_S2,  STN_KL,  STN_WL,  STN_RL,  STN_ST2, STN_ST4, STN_RR,  STN_BR,  STN_GR,  STN_SR,  STN_ZR ,
                                        STN_A,   STN_O,   XXXXXXX, XXXXXXX, STN_E,   STN_U
-    //     ),
+        ),
     // [L_STENO] = LAYOUT_split_3x6_3(
     //     KC_F24, KC_F24, KC_F24, KC_F24, KC_F24, KC_F24, KC_F24, KC_F24, KC_F24, KC_F24, KC_F24, TO(L_BASE),
     //
